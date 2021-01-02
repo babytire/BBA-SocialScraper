b28fed06c8b1ab0f36bf0c5e059478cae124474a
James West <james.west2@maine.edu>
2021-01-02 13:03:42 -0500

    Initial Email Input Box

--- my-app/src/components/LoginPage.js
+++ my-app/src/components/LoginPage.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 
 export default class LoginPage extends Component {
 
-    title = this.props.title;
+    submitUserInfo(e){
+        
+    }
 
     render() {
         return (
             <div>
                 { this.props.title }
-                
+                <form action={this.submitUserInfo()}>
+                <input type="emaiil" id="emailInputBox" minLength='5' maxLength='50' pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,}$" >
+                    
+                </input>
+                <input type="submit" id="loginButton"></input>
+                </form>
             </div>
         )
     }
