0ff7232903b78e903fd6c221d2e1ed8ae5c3e5ce
James West <james.west2@maine.edu>
2021-01-03 16:13:46 -0500

    Initial form
    +Email input box
    +Password input box
    +Forgot Password button
    +Login button
    +Initial CSS Styling

--- my-app/src/components/LoginPage.js
+++ my-app/src/components/LoginPage.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react'
+import './css/LoginPage.css'
 
 export default class LoginPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            submitted: false,
+            title: 'Scraper Log In'
+        };
+        this.submitUserInfo = this.submitUserInfo.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
 
-    submitUserInfo(e){
-        
+    submitUserInfo(event){
+        // pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,}$"
+        console.log(this.state.email);
+        event.preventDefault();
+    }
+
+    handleEmailChange(event){
+        this.setState({email: event.target.value});
+    }
+
+    handlePasswordChange(event){
+        this.setState({password: event.target.value});
     }
 
     render() {
         return (
             <div>
-                { this.props.title }
-                <form action={this.submitUserInfo()}>
-                <input type="emaiil" id="emailInputBox" minLength='5' maxLength='50' pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,}$" >
-                    
-                </input>
-                <input type="submit" id="loginButton"></input>
+                {this.state.title}
+                <form onSubmit={this.submitUserInfo} className="loginForm">
+                    <input type="email" value={this.state.email} onChange={this.handleEmailChange} className="emailInputBox" />
+                    <input type="password" value={this.state.password} onChange={this.handlePasswordChange} className="passwordInputBox" />
+                    <input type="button" value="Forgot Password" className="forgotPasswordButton" />
+                    <input type="submit" value="Login" className="loginButton" />
                 </form>
             </div>
         )
