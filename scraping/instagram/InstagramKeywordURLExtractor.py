a2cb6830364ee790e89b92000a573d6d971432aa
babytire <handlonr@gmail.com>
2021-02-08 12:45:35 -0500

    New InstagramKeywordURLExtractor

--- scraping/instagram/InstagramKeywordURLExtractor.py
+++ scraping/instagram/InstagramKeywordURLExtractor.py
@@ -1,32 +1,61 @@
-from selenium.webdriver import Chrome
+from time import sleep
 from selenium import webdriver
 import re
 
-
-pixels = 1000000000 #number of pixels wanted to scroll down
+pixels = 1080   #number of pixels to scroll down
+posts = 200     #number of posts to scrape
 page = 'https://www.instagram.com/explore/tags/minecraft/' # link to explre page with search results
+listOfLinks = [] #List of links being scraped
+regex = re.compile('(https:\/\/www\.instagram\.com\/p\/)(\w|_){11}(\/)')  #regex for matching only links to posts
 
-driver = webdriver.Chrome(executable_path = 'C:/Users/ryanh/Desktop/School/COS 397/Python/chromedriver_win32/chromedriver.exe')
+#Load Chromedriver
+browser = webdriver.Chrome(executable_path = 'C:/path/to/chromedriver.exe') #Replace with path to chromedriver.exe
+browser.implicitly_wait(5)
 
-driver.maximize_window()
+#Load and login to Instagram
+browser.get('https://www.instagram.com/')
 
-driver.get(page)
-#print(driver.title)
+sleep(2)
 
-driver.execute_script("window.scrollTo(0, " + str(pixels) + ")")
+username_input = browser.find_element_by_css_selector("input[name='username']")
+password_input = browser.find_element_by_css_selector("input[name='password']")
 
-#html = driver.page_source
-#html = driver.execute_script("return document.body.innerHTML;")
-html = driver.execute_script("return document.body.outerHTML;")
+username_input.send_keys("Username") #Replace 'Username' with Instagram username
+password_input.send_keys("Password") #Replace 'Password' with Instagram password
 
-#print(html)
+login_button = browser.find_element_by_xpath("//button[@type='submit']")
+login_button.click()
 
-regex = re.compile('(\/p\/)(\w|_){11}(\/)')
+sleep(5)
 
-f = open("innerhtmlbodyoutput.txt", "w", encoding= 'utf-8')
-f.write(html)
+#Load page to be scraped
+browser.get(page)
+
+#Iterate till requested # of posts scraped
+x = 0
+while(x < posts):
+    #scroll down page, get links
+    browser.execute_script("window.scrollBy(0, " + str(pixels) + ")")
+
+    elems = browser.find_elements_by_xpath("//a[@href]")
+
+    #Sometimes theres an error where the page hasn't loaded posts yet, so you have to wait a bit. Thats why theres this try catch block.
+    try:
+
+        #for each link, if its a link to a post and no been scraped yet, add it to the list
+        for elem in elems:
+            if elem.get_attribute("href") not in listOfLinks and regex.match(elem.get_attribute("href")) != None:
+                listOfLinks.append(elem.get_attribute("href"))
+                x += 1
+                print(x)
+    except:
+        sleep(0.1)
+
+#Write scraped post links to URLFrontier text document
+f = open("URLFrontier.txt", "w", encoding= 'utf-8')
+
+for x in listOfLinks:   
+    f.write(x + "\n")
 f.close()
 
-print(regex.match(html))
-
-driver.quit()
+browser.close()
