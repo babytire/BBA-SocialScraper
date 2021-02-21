ee73d0db9ce1c5239b8597d2ce3a3324239bf8e1
James West <james.west2@maine.edu>
2021-02-21 09:28:28 -0500

    breaking down React components

---
+++ my-app/src/components/SearchCriteriaInput.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import './css/SearchCriteriaInput.css'
+
+export default class SearchCriteriaInput extends Component {
+    constructor(){
+        super(props);
+        this.state{
+            searchCriteria: ''
+        }
+
+        this.handleSearchCriteriaInput = this.handleSearchCriteriaInput.bind(this);
+    }
+
+    handleSearchCriteriaInput(event){
+        this.setState({searchCriteria: event.target.value})
+    }
+    render() {
+        return (
+            <div className="searchCriteriaInputContainer">
+                <div className="searchCriteriaLabelContainer">
+                    <text className="searchCriteriaLabel">
+                        {this.props.label}
+                    </text>
+                </div>
+                <div className="searchCriteriaExampleContainer">
+                    <text className="searchCriteriaExample">
+                        {this.props.searchCriteriaExample}
+                        {/* Example: #dog#cat#blackbear */}
+                    </text>
+                </div>
+                <input type="search" className="searchCriteria" value={this.state.searchCriteria} onChange={this.handleSearchCriteriaInput} placeholder={this.props.searchCriteriaPlaceHolder}>
+                </input>
+            </div>
+        )
+    }
+}
