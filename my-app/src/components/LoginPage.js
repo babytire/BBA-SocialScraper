b44c8929243bc56d34ba066ba70e42fbaf60a4e5
James West <james.west2@maine.edu>
2021-03-11 19:07:08 -0500

    HomeButton

--- my-app/src/components/LoginPage.js
+++ my-app/src/components/LoginPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './css/LoginPage.css'
+import { Link, Redirect } from 'react-router-dom'
 
 export default class LoginPage extends Component {
     constructor(props){
@@ -8,25 +9,54 @@ export default class LoginPage extends Component {
             email: '',
             password: '',
             submitted: false,
-            title: 'Scraper Log In'
+            title: 'Scraper Log In',
+            link: true
         };
 
         const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,}$$/;
-
+        const homePage = '/HomePage'
+        const loginPage = '/LoginPage'
 
         this.handleLogin = this.handleLogin.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
-        this.handleCreate = this.handleCreate.bind(this);
     }
+
+    // const LinkToHome = ({ children, to, condition }) => (!!condition && to)
+    // ? <Link to={to}>{children}</Link>
+    // : <>{children}</>;
 
     handleLogin(event){
-        // 
-        console.log(this.state.email);
-        event.preventDefault();
+        const fetchUrl = '/api/loginClicked/';
+
+        fetch(fetchUrl, {
+            method: 'POST',
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password
+            })
+        })
+        .then((response) => response.json())
+        .then(function(data) {
+            if(data.result == 'OK')
+                {
+                // Redirect them to the Homepage
+                console.log("Redirecting to HomePage");
+                this.setState({link: true});
+                }
+            else
+                {
+                // Redirect them to the LoginPage
+                console.log("Redirecting to LoginPage");
+                }
+            })
+            .catch(function(error) {
+                console.log(error);
+            })
+            event.preventDefault();
     }
 
-    handleEmailChange(event){
+   handleEmailChange(event){
         this.setState({email: event.target.value});
     }
 
@@ -34,28 +64,34 @@ export default class LoginPage extends Component {
         this.setState({password: event.target.value});
     }
 
-    handleCreate(event){
-
-    }
-
     render() {
         return (
+        
+        <div className="loginPageContent">
+            <div className="loginPageTitleContainer">
+                <text className="loginPageTitle">
+                    {this.props.title}
+                </text>
+            </div>
             <div className="loginPageContainer">
                 <div className="contactUsContainer">
-                    <button onClick={this.handleContactUs} className="contactUsButton">Contact Us</button>
+                    <Link to='/ContactUs'>
+                        <button className="contactUsButton">Contact Us</button>
+                    </Link>
                 </div>
                 <div className="loginFormContainer">
-                    <form onSubmit={this.handleLogin} className="loginForm">
+                    <form className="loginForm" onSubmit={this.handleLogin}>
                         <input type="email" value={this.state.email} required requirederror="Email Address required." validate={this.emailRegex} validateerror="Please provide a valid email address." placeholder="Email" onChange={this.handleEmailChange} className="emailInputBox" />
-                        <input type="password" value={this.state.password} placeholder="Password" onChange={this.handlePasswordChange} className="passwordInputBox" />
+                        {/* <input type="password" value={this.state.password} placeholder="Password" onChange={this.handlePasswordChange} className="passwordInputBox" /> */}
                         <input type="button" value="Forgot Password" className="forgotPasswordButton" />
-                        <input type="submit" value="Login" className="loginButton" />
+                        <input type="submit"></input>
                     </form>
                 </div>
-                <div className="createContainer">
-                    <button onClick={this.handleCreate} className="createButton">Create</button>
-                </div>
+                <Link to='/RegisterAccount' className="createContainer">
+                    <button className="createButton">Create</button>
+                </Link>
             </div>
+        </div>
         )
     }
 }
